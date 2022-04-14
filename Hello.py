55136729f38298a701f6d2945ff6239e90f1c410
nipursinghpal <nipurpal@appventurez.com>
2022-04-14 17:22:09 +0530

    add host name port name

--- Hello.py
+++ Hello.py
@@ -78,7 +78,7 @@ import os
 SECRET_KEY = os.getenv("SECURITY")
 print(SECRET_KEY,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(debug=True,port=8000,host="0.0.0.0")
 
 
 
